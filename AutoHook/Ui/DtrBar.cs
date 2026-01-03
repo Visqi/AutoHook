9cc7231ec1cf8034e9a20e873b8c1e91133ebb57
dec <2226816+Deckerz@users.noreply.github.com>
2026-01-03 11:39:00 +0000

    format

--- AutoHook/Ui/DtrBar.cs
+++ AutoHook/Ui/DtrBar.cs
@@ -11,53 +11,58 @@ namespace AutoHook.Ui;
 public sealed class DtrBar : IDisposable
 {
     private string _text = string.Empty;
+    private readonly PluginUi _pluginUi;
     private readonly IDtrBarEntry _dtrBarEntry;
 
     public DtrBar(IDtrBar dtrBar, PluginUi pluginUi)
     {
+        _pluginUi = pluginUi;
         _dtrBarEntry = dtrBar.Get("AutoHook");
-        _dtrBarEntry.OnClick += (DtrInteractionEvent @event) =>
+        _dtrBarEntry.OnClick += HandleClick;
+    }
+
+    private void HandleClick(DtrInteractionEvent @event)
+    {
+        if (@event.ModifierKeys == ClickModifierKeys.None)
         {
-            if (@event.ModifierKeys == ClickModifierKeys.None)
+            if (@event.ClickType == MouseClickType.Left)
             {
-                if (@event.ClickType == MouseClickType.Left)
-                {
-                    Service.Configuration.PluginEnabled = !Service.Configuration.PluginEnabled;
-                    Service.Configuration.Save();
-                }
-                else if (@event.ClickType == MouseClickType.Right)
-                {
-                    pluginUi.Toggle();
-                }
+                Service.Configuration.PluginEnabled = !Service.Configuration.PluginEnabled;
+                Service.Configuration.Save();
             }
-            else
+            else if (@event.ClickType == MouseClickType.Right)
             {
-                if (@event.ClickType == MouseClickType.Left)
-                {
-                    if (Service.Configuration.HookPresets.SelectedPreset == null) return;
-                    var totalPresets = Service.Configuration.HookPresets.CustomPresets.Count;
-                    var selectedPresetIndex = Service.Configuration.HookPresets.CustomPresets.IndexOf(Service.Configuration.HookPresets.SelectedPreset!);
-                    Service.Configuration.HookPresets.SelectedPreset = Service.Configuration.HookPresets.CustomPresets[(selectedPresetIndex - 1) % totalPresets];
-
-                    Service.Configuration.Save();
-                }
-                else if (@event.ClickType == MouseClickType.Right)
-                {
-                    if (Service.Configuration.HookPresets.SelectedPreset == null) return;
-                    var totalPresets = Service.Configuration.HookPresets.CustomPresets.Count;
-                    var selectedPresetIndex = Service.Configuration.HookPresets.CustomPresets.IndexOf(Service.Configuration.HookPresets.SelectedPreset!);
-                    Service.Configuration.HookPresets.SelectedPreset = Service.Configuration.HookPresets.CustomPresets[(selectedPresetIndex + 1) % totalPresets];
-
-                    Service.Configuration.Save();
-                }
+                _pluginUi.Toggle();
             }
-        };
+        }
+        else
+        {
+            if (@event.ClickType == MouseClickType.Left)
+            {
+                if (Service.Configuration.HookPresets.SelectedPreset == null) return;
+                var totalPresets = Service.Configuration.HookPresets.CustomPresets.Count;
+                var selectedPresetIndex = Service.Configuration.HookPresets.CustomPresets.IndexOf(Service.Configuration.HookPresets.SelectedPreset!);
+                Service.Configuration.HookPresets.SelectedPreset = Service.Configuration.HookPresets.CustomPresets[(selectedPresetIndex - 1) % totalPresets];
+
+                Service.Configuration.Save();
+            }
+            else if (@event.ClickType == MouseClickType.Right)
+            {
+                if (Service.Configuration.HookPresets.SelectedPreset == null) return;
+                var totalPresets = Service.Configuration.HookPresets.CustomPresets.Count;
+                var selectedPresetIndex = Service.Configuration.HookPresets.CustomPresets.IndexOf(Service.Configuration.HookPresets.SelectedPreset!);
+                Service.Configuration.HookPresets.SelectedPreset = Service.Configuration.HookPresets.CustomPresets[(selectedPresetIndex + 1) % totalPresets];
+
+                Service.Configuration.Save();
+            }
+        }
     }
 
     public void Dispose()
     {
         if (_dtrBarEntry != null)
         {
+            _dtrBarEntry.OnClick -= HandleClick;
             Clear();
         }
     }
