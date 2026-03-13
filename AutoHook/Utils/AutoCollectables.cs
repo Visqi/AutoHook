b99c2952e6baf2094430bbbf8ed5816639e6212c
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-03-13 17:01:19 +0000

    Update AutoCollectables.cs

--- AutoHook/Utils/AutoCollectables.cs
+++ AutoHook/Utils/AutoCollectables.cs
@@ -4,6 +4,7 @@ using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
 using Dalamud.Game.Text;
 using Dalamud.Utility;
 using FFXIVClientStructs.FFXIV.Client.UI;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 using Lumina.Excel.Sheets;
 using System.Text.RegularExpressions;
 
@@ -57,11 +58,11 @@ public class AutoCollectables : IDisposable {
         if (FindRow<Item>(x => x.IsCollectable && !x.Singular.IsEmpty && name.Contains(x.Singular.GetText(), StringComparison.InvariantCultureIgnoreCase)) is not { RowId: > 0 } item)
             return;
 
-        Svc.Log.Debug($"[AutoCollectables] Detected item [{item}] {item.Name}");
+        Svc.Log.Debug($"[AutoCollectables] Detected item [#{item.RowId}] {item.Name}");
 
         if (forceNo) {
-            Svc.Log.Debug($"[AutoCollectables] Force NO for [{item.RowId}] {item.Name}");
-            AddonSelectYesno.No();
+            Svc.Log.Debug($"[AutoCollectables] Force NO for [#{item.RowId}] {item.Name}");
+            No(addon);
             return;
         }
 
@@ -72,26 +73,38 @@ public class AutoCollectables : IDisposable {
             var min = collectability.CollectablesShopRefine.Value.LowCollectability;
             Svc.Log.Debug($"[AutoCollectables] Minimum collectability required is {min}, value detected is {value}");
             if (value >= min) {
-                Svc.Log.Debug($"[AutoCollectables] Entry is [{item}] {item.Name} with a sufficient collectability of {value}");
-                AddonSelectYesno.Yes();
+                Svc.Log.Debug($"[AutoCollectables] Entry is [#{item.RowId}] {item.Name} with a sufficient collectability of {value}");
+                Yes(addon);
             }
             else {
-                Svc.Log.Debug($"[AutoCollectables] Entry is [{item}] {item.Name} with an insufficient collectability of {value}");
-                AddonSelectYesno.No();
+                Svc.Log.Debug($"[AutoCollectables] Entry is [#{item.RowId}] {item.Name} with an insufficient collectability of {value}");
+                No(addon);
             }
         }
         else {
             if (item.AetherialReduce > 0) {
                 Svc.Log.Debug($"[AutoCollectables] Entry is [#{item.RowId}] {item.Name} and probably an aethersand fish. Skipping collectability check.");
-                AddonSelectYesno.Yes();
+                Yes(addon);
             }
             else if (TryGetRow<WKSItemInfo>(item.AdditionalData.RowId, out var wksItem)) {
                 Svc.Log.Debug($"[AutoCollectables] Entry is [#{item.RowId}] {item.Name} for {wksItem.WKSItemSubCategory.ValueNullable?.Name ?? "null"}. Skipping collectability check.");
-                AddonSelectYesno.Yes();
+                Yes(addon);
             }
             else {
-                Svc.Log.Debug($"[AutoCollectables] Failed to find matching CollectablesShopItem for [{item.RowId}] {item.Name}. Not an aethersand fish or a CE fish.");
+                Svc.Log.Debug($"[AutoCollectables] Failed to find matching CollectablesShopItem for [#{item.RowId}] {item.Name}. Not an aethersand fish or a CE fish.");
             }
         }
+    }
+
+    public static unsafe void Yes(AddonSelectYesno* addon) {
+        var evt = new AtkEvent() { Listener = &addon->AtkUnitBase.AtkEventListener, Target = &AtkStage.Instance()->AtkEventTarget };
+        var data = new AtkEventData();
+        addon->ReceiveEvent(AtkEventType.ButtonClick, 0, &evt, &data);
+    }
+
+    public static unsafe void No(AddonSelectYesno* addon) {
+        var evt = new AtkEvent() { Listener = &addon->AtkUnitBase.AtkEventListener, Target = &AtkStage.Instance()->AtkEventTarget };
+        var data = new AtkEventData();
+        addon->ReceiveEvent(AtkEventType.ButtonClick, 1, &evt, &data);
     }
 }
