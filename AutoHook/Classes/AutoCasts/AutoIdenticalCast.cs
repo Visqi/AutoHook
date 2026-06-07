4ee767ad62298847afb119d170cefd42f80fc05f
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2026-06-08 00:12:57 +0100

    we're getting rid of this

--- AutoHook/Classes/AutoCasts/AutoIdenticalCast.cs
+++ AutoHook/Classes/AutoCasts/AutoIdenticalCast.cs
@@ -3,8 +3,6 @@ using FFXIVClientStructs.FFXIV.Client.Game;
 namespace AutoHook.Classes.AutoCasts;
 
 public sealed class AutoIdenticalCast : BaseActionCast {
-    public override bool DoesCancelMooch() => true;
-
     public AutoIdenticalCast() : base(IDs.Actions.IdenticalCast, ActionType.Action) { }
 
     public override string GetName() => UIStrings.Identical_Cast;
@@ -15,10 +13,7 @@ public sealed class AutoIdenticalCast : BaseActionCast {
         && !Service.WorldState.HasStatus(IDs.Status.IdenticalCast)
         && !Service.WorldState.HasStatus(IDs.Status.SurfaceSlap);
 
-    protected override DrawOptionsDelegate DrawOptions => () => {
-        DrawAutoCastConditions();
-        DrawMoochCancelOption();
-    };
+    protected override DrawOptionsDelegate DrawOptions => () => DrawAutoCastConditions();
 
     public override int Priority { get; set; } = 8;
     public override bool IsExcludedPriority { get; set; } = false;
